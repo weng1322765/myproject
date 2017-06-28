b2333c12b56346bca09d6dd2c1a9328345b4eddc
weng1322765 <342506411@qq.com>
2017-06-28 16:22:30 +0800

    new

--- my_spring_mvc_v2.0/src/test/java/MyTest.java
+++ my_spring_mvc_v2.0/src/test/java/MyTest.java
@@ -1,19 +1,26 @@
+import com.alibaba.druid.support.logging.Log;
+import com.service.StudentService;
 import junit.framework.TestCase;
 import org.junit.Test;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * Created by WengLei on 2017/6/19.
  */
-public class MyTest extends TestCase{
+public class MyTest extends BaseTest{
 
     public void forFun(){
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
         System.out.println(stackTrace[1].getClassName());
     }
 
+    @Autowired
+    StudentService studentService;
+
     @Test
     public void testMain(){
-
+        System.out.println(studentService.getAllStudents().size());
     }
 }
